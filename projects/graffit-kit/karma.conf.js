b9c05094ef2fed24326773cb5dfa6edcfabc6340
MACTEPwar <shebanic95@gmail.com>
2022-04-30 20:02:57 +0300

    подготовка проекта

--- projects/graffit-kit/karma.conf.js
+++ projects/graffit-kit/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/grapffit-kit'),
+      dir: require('path').join(__dirname, '../../coverage/graffit-kit'),
       subdir: '.',
       reporters: [
         { type: 'html' },
